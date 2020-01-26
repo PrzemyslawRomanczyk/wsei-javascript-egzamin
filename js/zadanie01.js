12b8a9cd2855af19c3d6102d4e8d5b302957ce7c
PrzemyslawRomanczyk <49125731+PrzemyslawRomanczyk@users.noreply.github.com>
2020-01-26 12:10:13 +0100

    Update taks

--- js/zadanie01.js
+++ js/zadanie01.js
@@ -3,3 +3,17 @@ function getSqrt(n) {
 }
 console.log(getSqrt(4));
 console.log(getSqrt(16));
+
+
+
+///Definicje
+
+//DOM - Document Object Model - Ilustracja elementów znajdujących się na stronie internetowej.
+// Na podstawie DOM przeglądarka wyświetla elementy na stronie internetowej. Za pomocą obiektów w DOMie 
+// możemy edytować i odziaływać na stronę internetową poprzez język java script
+
+//Kod źródłowy - Zbiór poleceń zapisancyh w język programowania który może zostać skompilowany i
+// uruchomiony przez komputer. 
+
+//Algorytm –skończony ciąg jasno zdefiniowanychczynności
+// koniecznych do wykonania pewnegorodzaju zadań.
--- js/zadanie06.js
+++ js/zadanie06.js
@@ -1,11 +1,20 @@
-var button_1 = document.getElementById("button-1");
-let button_2 = document.getElementById("button-2");
-let button_3 = document.getElementById("button-3");
-let productList = document.getElementById("shopping-list");
-let new_lm = document.createElement("li");
+window.onload = () => {
+    let list = document.getElementById('shopping-list')
 
-button_1.addEventListener("click", function(){ 
-    new_lm.innerHTML = "Chleb"
-    productList.appendChild(new_lm);
+    document.getElementById('button-1').onclick = () => {
+        let li = document.createElement('LI')
+        li.appendChild(document.createTextNode('Chleb'))
+        list.appendChild(li)
+    }
 
-});
+    document.getElementById('button-2').onclick = () => {
+        list.removeChild(list.lastElementChild)
+    }
+
+    document.getElementById('button-3').onclick = () => {
+        let child = list.children[1].innerText
+        let li = document.createElement('LI')
+        li.appendChild(document.createTextNode(child))
+        list.appendChild(li)
+    }
+}
